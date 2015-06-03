c55e4f58770484909d2792f038dc3786cda25313
coderbhupendra <birendrachauhan3@gmail.com>
2015-06-03 23:49:37 +0000

    using RNN to do matrix mutiplication

--- RNN_forMatrix_mutiplication/mat_rnn.py
+++ RNN_forMatrix_mutiplication/mat_rnn.py
@@ -18,9 +18,9 @@ class RNN(object):
 		b_hh = np.zeros((n_hidden,), dtype=theano.config.floatX)
 		b_hy = np.zeros((nout,), dtype=theano.config.floatX)
 		self.activ = T.nnet.sigmoid
-		lr = T.scalar()
-		u = T.matrix()
-		t = T.scalar()
+		lr = T.fscalar()
+		w = T.fmatrix()
+		t = T.fvector()
  
 		W_uh = theano.shared(W_uh, 'W_uh')
 		W_hh = theano.shared(W_hh, 'W_hh')
@@ -38,19 +38,17 @@ class RNN(object):
 
 		h0_tm1 = theano.shared(np.zeros(n_hidden, dtype=theano.config.floatX))
  
-		h, _ = theano.scan(self.recurrent_fn, sequences = u,
+		h, _ = theano.scan(self.recurrent_fn, sequences = w,
 					   outputs_info = [h0_tm1],
 					   non_sequences = [W_hh, W_uh, W_hy, b_hh])
  
 		y = T.dot(h[-1], W_hy) + b_hy
-
 		cost = ((t - y)**2).mean(axis=0).sum()
- 
-		gW_hh, gW_uh, gW_hy,\
+ 		gW_hh, gW_uh, gW_hy,\
 		   gb_hh, gb_hy = T.grad(
 			   cost, [W_hh, W_uh, W_hy, b_hh, b_hy])
  
-		self.train_step = theano.function([u, t, lr], cost,
+		self.train_step = theano.function([w,t, lr], cost,
 							on_unused_input='warn',
 							updates=[(W_hh, W_hh - lr*gW_hh),
 									 (W_uh, W_uh - lr*gW_uh),
@@ -61,29 +59,25 @@ class RNN(object):
 		
 	def recurrent_fn(self, u_t, h_tm1, W_hh, W_uh, W_hy, b_hh):
 		h_t = self.activ(T.dot(h_tm1, W_hh) + T.dot(u_t, W_uh) + b_hh)
-		"""rng = np.random.RandomState(1234)
-		print(h_t.eval
-			({
-			h_tm1:np.random.random((20,))
-			,W_hh:np.random.random((20,20))
-			,u_t:np.random.random((10,2))
-			, W_uh:np.random.random((2,20))
-			,b_hh:np.random.random((20,))
-			}))"""
+		
 		return h_t
 
 	def get_param(self):
 		return self.param
 
 if __name__ == '__main__':
-	rnn = RNN(2, 20, 1)
+	rnn = RNN(6, 20, 9)
 	lr = 0.1
 	e = 1
 	vals = []
 	for i in xrange(int(5e5)):
-		u = np.random.rand(10,2)
-		t = np.dot(u[:,0], u[:,1])
-		c = rnn.train_step(u, t, lr)
+		u=np.random.rand(3,3)
+		v=np.random.rand(3,3)
+		w=np.concatenate((u,v),axis=1)
+
+		t = (np.dot(u,v)).flatten()
+
+		c = rnn.train_step(w,t, lr)
 		print "iteration {0}: {1}".format(i, np.sqrt(c))
 		e = 0.1*np.sqrt(c) + 0.9*e
 		if i % 1000 == 0:
--- RNN_forMatrix_mutiplication/mat_rnn_test.py
+++ RNN_forMatrix_mutiplication/mat_rnn_test.py
@@ -4,16 +4,18 @@ import numpy as np
 import os.path
 import cPickle,pickle
 import random
+import matplotlib.pyplot as plt
  
 class RNN(object):
  
 	def __init__(self, nin, n_hidden, nout):
 		
-		self.activ = T.nnet.sigmoid
+
+
 		lr = T.scalar()
-		u = T.matrix()
-		t = T.scalar()
- 
+		w = T.matrix()
+		t = T.vector()
+ 		self.activ = T.nnet.sigmoid
 		if os.path.exists('W_uh.pkl'):
 			with open('W_uh.pkl') as f:
 				print"yes weights found"
@@ -33,36 +35,44 @@ class RNN(object):
 				b_hy = w5
 		else:    print"no weights found"
 
-		 
-		
+
 		h0_tm1 = theano.shared(np.zeros(n_hidden, dtype=theano.config.floatX))
  
-		h, _ = theano.scan(self.recurrent_fn, sequences = u,
+		h, _ = theano.scan(self.recurrent_fn, sequences = w,
 					   outputs_info = [h0_tm1],
 					   non_sequences = [W_hh, W_uh, W_hy, b_hh])
  
 		y = T.dot(h[-1], W_hy) + b_hy
+
 		cost = y
  
-		self.train_step = theano.function([u], cost,
+		self.train_step = theano.function([w], cost,
 							on_unused_input='warn',
 							
 							allow_input_downcast=True)
 		
 	def recurrent_fn(self, u_t, h_tm1, W_hh, W_uh, W_hy, b_hh):
 		h_t = self.activ(T.dot(h_tm1, W_hh) + T.dot(u_t, W_uh) + b_hh)
+		
 		return h_t
-	
+
+	def get_param(self):
+		return self.param
 
 if __name__ == '__main__':
-	rnn = RNN(2, 20, 1)
-	
+	rnn = RNN(6, 20, 9)
+	lr = 0.01
+	e = 1
+	vals = []
 	for i in xrange(int(5)):
-		u = np.random.rand(10,2)
-		t = np.dot(u[:,0], u[:,1])
-		c = rnn.train_step(u)
-		print "\niteration {0} : \n{1} : {2} : {3} : {4} ".format(i,u,t,c,(t-c))
+		u=np.random.rand(3,3)
+		v=np.random.rand(3,3)
+		w=np.concatenate((u,v),axis=1)
+
+		t = (np.dot(u,v)).flatten()
+
+		c = rnn.train_step(w)
+		print "iteration {0} \n: {1} \n:{2}\n".format(i,t,c)
 		
 
-	
 
